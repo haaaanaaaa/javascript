2b07cdc97e66d26acbb4734d6afe2487f886f83a
平賀はな <hanahiraga@gmail.com>
2022-11-08 13:49:14 +0900

    課題提出_1回目

---
+++ app.js
@@ -0,0 +1,8 @@
+for (let i = 1; i < 51; i++) {
+  if (i % 10 == 0) {
+    console.log("今" + i + "回ループしました");
+  } else if (i % 4 == 0) {
+    console.log("4で割れる数です" + i);
+  }
+}
+alert("50回カウントが終わりました");
